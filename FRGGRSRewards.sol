ff59998105e860dd549bafe15f14a96d10358d67
happibutzad <91788967+happibutzad@users.noreply.github.com>
2022-04-13 12:41:29 +0800

    Update FRGGRSRewards.sol

--- FRGGRSRewards.sol
+++ FRGGRSRewards.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract FLY is ERC20, ERC20Burnable, Ownable {
+contract FLYRewards is ERC20, ERC20Burnable, Ownable {
 
   mapping(address => bool) controllers;
   
-  constructor() ERC20("Froggers Fly", "FLY") { }
+  constructor() ERC20("FroggersFly", "FLY") { }
 
   function mint(address to, uint256 amount) external {
     require(controllers[msg.sender], "Only controllers can mint");
